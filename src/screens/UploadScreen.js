fe752eae550cd42a71bda89176ad2b04a1fdcca2
royleochan <royleochan@gmail.com>
2020-12-14 22:02:19 +0800

    Add image picking logic

--- src/screens/UploadScreen.js
+++ src/screens/UploadScreen.js
@@ -1,27 +1,73 @@
-import React from "react";
-import { View, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Button,
+  Image,
+} from "react-native";
+import { useActionSheet } from "@expo/react-native-action-sheet";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
+import { takeImage, chooseFromLibrary } from "utils/imagePicker";
 import Colors from "constants/Colors";
 import FemaleCategories from "constants/FemaleCategories";
 import MaleCategories from "constants/MaleCategories";
 import DefaultText from "components/DefaultText";
 import DropDown from "components/DropDown";
+import MainButton from "components/MainButton";
 
 const UploadScreen = (props) => {
+  const { showActionSheetWithOptions } = useActionSheet();
+  const [pickedImage, setPickedImage] = useState();
+
+  const showActionSheet = () => {
+    const options = ["Take Photo", "Choose From Library", "Cancel"];
+    const cancelButtonIndex = 2;
+
+    showActionSheetWithOptions(
+      {
+        title: "Choose an action",
+        options,
+        cancelButtonIndex,
+        tintColor: Colors.primary,
+      },
+      async (buttonIndex) => {
+        let selectedImage;
+        if (buttonIndex === 0) {
+          selectedImage = await takeImage();
+        } else if (buttonIndex === 1) {
+          selectedImage = await chooseFromLibrary();
+        }
+        setPickedImage(selectedImage);
+      }
+    );
+  };
+
   return (
     <View style={styles.screenContainer}>
       <View style={styles.imageContainer}>
         <View style={styles.imagePreview}>
-          <TouchableOpacity
-            style={styles.iconContainer}
-            onPress={() => {
-              console.log("Select Picture");
-            }}
-          >
-            <AntDesign name="plussquareo" size={24} color={Colors.primary} />
-          </TouchableOpacity>
+          {pickedImage === undefined ? (
+            <TouchableOpacity
+              style={styles.iconContainer}
+              onPress={showActionSheet}
+            >
+              <AntDesign name="plussquareo" size={24} color={Colors.primary} />
+            </TouchableOpacity>
+          ) : (
+            <Image style={styles.image} source={{ uri: pickedImage.uri }} />
+          )}
         </View>
+        {pickedImage !== undefined && (
+          <MainButton
+            style={styles.button}
+            onPress={() => setPickedImage(undefined)}
+          >
+            Cancel
+          </MainButton>
+        )}
       </View>
       <View style={styles.formContainer}>
         <View style={styles.formBoxContainer}>
@@ -60,6 +106,7 @@ const styles = StyleSheet.create({
   imageContainer: {
     height: "40%",
     justifyContent: "center",
+    alignItems: "center"
   },
   imagePreview: {
     width: 200,
@@ -69,6 +116,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderColor: Colors.gray,
     borderWidth: 3,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    width: 200,
+    height: 30,
+    marginTop: 12,
   },
   formContainer: {
     height: "65%",
