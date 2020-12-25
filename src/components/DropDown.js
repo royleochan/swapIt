688a0a26da610dc39649725b1a6cdf24df799269
royleochan <royleochan@gmail.com>
2020-12-26 02:54:25 +0800

    Update dropdown component

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -3,12 +3,16 @@ import { View, StyleSheet } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 
 const DropDown = (props) => {
-  const { placeholder, items } = props;
+  const { placeholder, items, functions } = props;
+  const [value, setValue] = functions;
   return (
     <View style={styles.container}>
       <RNPickerSelect
+        value={value}
         placeholder={{ label: placeholder, value: null, color: "#939AA4" }}
-        onValueChange={(value) => console.log(value)}
+        onValueChange={(value) => {
+          setValue(value);
+        }}
         items={items}
       />
     </View>
