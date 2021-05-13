b84cec1c08f23cde3ee7023e4aa01bf9a4e45cbb
pangpuncake <pangpuncake@gmail.com>
2021-05-13 16:04:09 +0800

    Fix bug of multiple event listeners but connection seems to not work

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,5 +1,5 @@
 import {REACT_APP_BACKEND_URL} from "@env";
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import {GiftedChat} from "react-native-gifted-chat";
 
 import {useSelector} from "react-redux";
@@ -7,9 +7,12 @@ import request from "utils/request";
 import io from "socket.io-client";
 
 const ChatScreen = (props) => {
-  const [socket, setSocket] = useState(io(`${REACT_APP_BACKEND_URL}/chatSocket`));
+  const [socket] = useState(io(`${REACT_APP_BACKEND_URL}/chatSocket`, {
+    autoConnect: false
+  }));
   const [messages, setMessages] = useState([]);
   const [lastSentMessage, setLastSentMessage] = useState("");
+  const firstUpdate = useRef(true);
 
   const loggedInUserId = useSelector((state) => state.auth.user.id);
   const userId = props.route.params._id;
@@ -34,31 +37,43 @@ const ChatScreen = (props) => {
     });
   };
 
-  useEffect(() => {
-    const loadMessages = async () => {
-      const result = await getMessages();
-      setMessages(result.reverse());
-    };
-    loadMessages();
-  }, []);
+  // useEffect(() => {
+  //   const loadMessages = async () => {
+  //     const result = await getMessages();
+  //     setMessages(result.reverse());
+  //   };
+  //   loadMessages();
+  // }, []);
 
   useEffect(() => {
     socket.connect();
     socket.on("connect", async () => {
+      console.log("Received connect, preparing to emit...");
       socket.emit("join", "609a8094dec46a7ce23a5e61");
     })
-    socket.on("joined", (roomId) => {
+    socket.on("joined", async (roomId) => {
       console.log("User has joined room %s", roomId);
+      await getMessages()
+          .then(response => setMessages(response.reverse()))
+          .catch(e => console.error(e));
     });
+    socket.on("message", (message) => {
+      console.log("INCOMING MESSAGE: " + message.content);
+    });
+    return () => {
+      console.log("DISCONNECT");
+      socket.disconnect();
+    };
   }, []);
 
-  socket.on('message', (message) => {
-    console.log("INCOMING MESSAGE: " + message.content);
-  });
-
   useEffect(() => {
-    console.log("LAST SENT:" + lastSentMessage);
-    socket.emit('message', { userId: loggedInUserId, message: lastSentMessage });
+    if (!firstUpdate.current) {
+      console.log("LAST SENT:" + lastSentMessage);
+      socket.emit("message", { userId: loggedInUserId, message: lastSentMessage });
+    } else {
+      firstUpdate.current = false;
+      console.log("INITIAL RENDER:", lastSentMessage);
+    }
   }, [lastSentMessage]);
 
   const onSend = useCallback((newMessage = []) => {
