41fdaadd5a864dabd0d5fadc8929ff31acdade6c
pangpuncake <pangpuncake@gmail.com>
2021-05-14 12:48:45 +0800

    Remove /chatSocket, might be issue to no socket connection

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,15 +1,17 @@
-import {REACT_APP_BACKEND_URL} from "@env";
-import React, {useCallback, useEffect, useRef, useState} from "react";
-import {GiftedChat} from "react-native-gifted-chat";
+import { REACT_APP_BACKEND_URL } from "@env";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
 
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import request from "utils/request";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
 const ChatScreen = (props) => {
-  const [socket] = useState(io(`${REACT_APP_BACKEND_URL}/chatSocket`, {
-    autoConnect: false
-  }));
+  const [socket] = useState(
+    io(`${REACT_APP_BACKEND_URL}`, {
+      autoConnect: false,
+    })
+  );
   const [messages, setMessages] = useState([]);
   const [lastSentMessage, setLastSentMessage] = useState("");
   const firstUpdate = useRef(true);
@@ -51,26 +53,28 @@ const ChatScreen = (props) => {
     socket.on("connect", async () => {
       console.log("Received connect, preparing to emit...");
       socket.emit("join", "609a8094dec46a7ce23a5e61");
-    })
+    });
     socket.on("joined", async (roomId) => {
       console.log("User has joined room %s", roomId);
       await getMessages()
-          .then(response => setMessages(response.reverse()))
-          .catch(e => console.error(e));
+        .then((response) => setMessages(response.reverse()))
+        .catch((e) => console.error(e));
     });
     socket.on("message", (message) => {
       console.log("INCOMING MESSAGE: " + message.content);
-      const newMessage = [{
-        _id: new Date().toDateString(),
-        text: message.content,
-        createdAt: new Date(),
-        user: {
-          _id: message.userId,
-          avatar: userProfilePic,
+      const newMessage = [
+        {
+          _id: new Date().toDateString(),
+          text: message.content,
+          createdAt: new Date(),
+          user: {
+            _id: message.userId,
+            avatar: userProfilePic,
+          },
         },
-      }];
+      ];
       setMessages((previousMessages) =>
-          GiftedChat.append(previousMessages, newMessage)
+        GiftedChat.append(previousMessages, newMessage)
       );
     });
     return () => {
@@ -82,7 +86,10 @@ const ChatScreen = (props) => {
   useEffect(() => {
     if (!firstUpdate.current) {
       console.log("LAST SENT:" + lastSentMessage);
-      socket.emit("message", { userId: loggedInUserId, message: lastSentMessage });
+      socket.emit("message", {
+        userId: loggedInUserId,
+        message: lastSentMessage,
+      });
     } else {
       firstUpdate.current = false;
     }
@@ -92,7 +99,7 @@ const ChatScreen = (props) => {
     console.log(newMessage[0]);
     setLastSentMessage(newMessage[0].text);
     setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, newMessage)
+      GiftedChat.append(previousMessages, newMessage)
     );
   }, []);
 
