56f922eb6099a0d205d35bb5f39f8f3bc94d3d3c
royleochan <royleochan@gmail.com>
2021-05-17 15:56:16 +0800

    Adjust image handling for chat

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,17 +1,18 @@
+import uuid from "react-native-uuid";
 import { REACT_APP_BACKEND_URL } from "@env";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Actions, GiftedChat } from "react-native-gifted-chat";
+import { useSelector } from "react-redux";
+import { io } from "socket.io-client";
+import { Icon } from "react-native-elements";
+
+import request from "utils/request";
+import Colors from "constants/Colors";
 import {
   takeImage,
   chooseFromLibrary,
   uploadImageHandler,
 } from "utils/imagePicker";
-import uuid from "react-native-uuid";
-
-import { useSelector } from "react-redux";
-import request from "utils/request";
-import { io } from "socket.io-client";
-import {Icon} from "react-native-elements";
 
 const ChatScreen = (props) => {
   const [socket] = useState(
@@ -22,34 +23,29 @@ const ChatScreen = (props) => {
   const [messages, setMessages] = useState([]);
   const [lastSentMessage, setLastSentMessage] = useState("");
   const [lastSentImage, setLastSentImage] = useState("");
-  // const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const firstUpdate = useRef(true);
 
   const loggedInUserId = useSelector((state) => state.auth.user.id);
   const userId = props.route.params._id;
   const userProfilePic = props.route.params.profilePic;
 
-  //Choose image from image library
-  const handlePickImage = () => {
-  }
-
-  //Take picture
-  const handleLaunchCamera = () => {
-  }
+  const handleCancel = () => {};
 
   const renderActions = (props) => {
     return (
-        <Actions
-            {...props}
-            options={{
-              ['Use camera']: handleLaunchCamera,
-              ['Choose Image']: handlePickImage,
-            }}
-            icon={() => (
-                <Icon name={'attachment'} size={23} color={"black"} />
-            )}
-            onSend={args => console.log(args)}
-        />
+      <Actions
+        {...props}
+        options={{
+          ["Use camera"]: takeImage,
+          ["Choose Image"]: chooseFromLibrary,
+          ["Cancel"]: handleCancel,
+        }}
+        icon={() => (
+          <Icon name={"attachment"} size={23} color={Colors.primary} />
+        )}
+        onSend={(args) => console.log(args)}
+      />
     );
   };
 
