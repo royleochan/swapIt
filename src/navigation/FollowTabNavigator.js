5267c7d5f9f6e040a0f3e207f5f9bdd384314b90
royleochan <royleochan@gmail.com>
2021-05-26 03:15:47 +0800

    Add follow top tab navigator

--- src/navigation/FollowTabNavigator.js
+++ src/navigation/FollowTabNavigator.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { useSelector } from "react-redux";
 
+import Colors from "constants/Colors";
 import FollowScreen from "screens/FollowScreen";
 
 const TopTab = createMaterialTopTabNavigator();
 
 const FollowTabNavigator = () => {
+  const user = useSelector((state) => state.auth.user);
+
   return (
-    <TopTab.Navigator>
+    <TopTab.Navigator
+      tabBarOptions={{
+        labelStyle: { fontSize: 12, fontFamily: "lato" },
+        activeTintColor: Colors.primary,
+        inactiveTintColor: Colors.glass,
+        indicatorStyle: { backgroundColor: Colors.primary },
+      }}
+    >
       <TopTab.Screen
         name="Followers"
         component={FollowScreen}
         initialParams={{ type: "followers" }}
+        options={{ title: `Followers (${user.followers.length})` }}
       />
       <TopTab.Screen
         name="Following"
         component={FollowScreen}
         initialParams={{ type: "following" }}
+        options={{ title: `Following (${user.following.length})` }}
       />
     </TopTab.Navigator>
   );
--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -2,16 +2,14 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import * as NavOptions from "navigation/options/DefaultNavOptions";
-import FollowTabNavigator from "navigation/FollowTabNavigator";
+import UserProfileNavigator from "navigation/UserProfileNavigator";
 import HomeScreen from "screens/HomeScreen";
 import ProductDetailsScreen from "screens/ProductDetailsScreen";
 import MessagesScreen from "screens/MessagesScreen";
 import ChatScreen from "screens/ChatScreen";
-import ProfileScreen from "screens/ProfileScreen";
 import CategoryScreen from "screens/CategoryScreen";
 import ResultsScreen from "screens/ResultsScreen";
 import SearchUserScreen from "screens/SearchUserScreen";
-import ReviewsScreen from "screens/ReviewsScreen";
 
 const Stack = createStackNavigator();
 
@@ -49,24 +47,14 @@ const HomeNavigator = () => {
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name="ProfileScreen"
-        component={ProfileScreen}
-        options={NavOptions.DefaultNavOptions}
-      />
-      <Stack.Screen
         name="Search"
         component={SearchUserScreen}
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name="Reviews"
-        component={ReviewsScreen}
-        options={NavOptions.DefaultNavOptions}
-      />
-      <Stack.Screen
-        name="Follow"
-        component={FollowTabNavigator}
-        options={NavOptions.DefaultNavOptions}
+        name="ProfileScreen"
+        component={UserProfileNavigator}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
