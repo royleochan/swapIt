89091eff0e85ccab594856f096295d04f4c57f79
royleochan <royleochan@gmail.com>
2021-06-29 00:44:24 +0800

    Add api call to fetch completed review

--- src/screens/profile/CompletedReviewScreen.js
+++ src/screens/profile/CompletedReviewScreen.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
 
-const CompletedReviewScreen = () => {
+import request from "utils/request";
+import Loader from "components/Loader";
+
+const CompletedReviewScreen = (props) => {
+  const { matchId } = props.route.params;
+  const loggedInUserId = useSelector((state) => state.auth.user.id);
+
+  const [review, setReview] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getReview = async () => {
+      try {
+        const response = await request.get(
+          `/api/reviews/${loggedInUserId}/${matchId}`
+        );
+        setReview(response.data.review);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    setIsLoading(true);
+    getReview();
+  }, []);
+
+  if (isLoading) {
+    return <Loader isLoading={isLoading} />;
+  }
+
   return (
     <View>
-      <Text>Review</Text>
+      <Text>{review.rating}</Text>
+      <Text>{review.description}</Text>
     </View>
   );
 };
