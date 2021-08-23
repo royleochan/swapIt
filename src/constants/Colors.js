e03a6c3f9f51de6a8d987e410899d432e761f107
royleochan <royleochan@gmail.com>
2021-08-23 17:15:29 +0800

    feat: change theme colors

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,11 +1,12 @@
 export default {
   primary: "#304267", // Dark Blue
-  background: "rgba(255, 255, 255, 0.7)", // White
+  background: "#FBFBFB", // White
   accent: "#FFA6B4", // Flamingo Pink
   glass: "#C4C4C4", // Opacity 20%, add drop shadow
-  textInput: 'rgba(196, 196, 196, 0.3)',
+  textInput: "rgba(196, 196, 196, 0.3)",
   gray: "#F1F1F1",
   star: "#FAE0AE",
   lightGray: "#F8F8F8",
   darkPink: "#B18197",
+  delete: "#B00000",
 };
